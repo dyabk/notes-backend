5f4c392240e99104a09e1b7d6957a75c92954bd4
dyabk <dzmitry.yabkevich@outlook.com>
2022-01-24 00:33:57 -0500

    After importing the module and creating the object, defined two routes to the application with the respective event handlers: one to the root and the other one to the notes. The event handlers have the request and response parameters

--- index.js
+++ index.js
@@ -22,11 +22,15 @@ let notes = [
     }
 ]
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type' : 'application/json' })
-    response.end(JSON.stringify(notes))
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
