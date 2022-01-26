0567de1264dfffc1e8b7fb269995940d1e8019ee
dyabk <dzmitry.yabkevich@outlook.com>
2022-01-26 14:04:45 -0500

    Set the server response to the code 404 when accessing a resource with a non-existing id

--- index.js
+++ index.js
@@ -32,12 +32,13 @@ app.get('/api/notes', (request, response) => {
 
 app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
-  const note = notes.find(note => {
-    console.log(note.id, typeof note.id, id, typeof id, note.id === id)
-    return note.id === id
-  })
-  console.log(note)
-  response.json(note)
+  const note = notes.find(note => note.id === id)
+  
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
 })
 
 const PORT = 3001
