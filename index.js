165d3051f769304b33cfbf89f0ed43c4417f445e
dyabk <dzmitry.yabkevich@outlook.com>
2022-01-26 13:33:32 -0500

    Added a console log inside the comparison function passed to the find function

--- index.js
+++ index.js
@@ -32,8 +32,10 @@ app.get('/api/notes', (request, response) => {
 
 app.get('/api/notes/:id', (request, response) => {
   const id = request.params.id
-  console.log(id)
-  const note = notes.find(note => note.id === id)
+  const note = notes.find(note => {
+    console.log(note.id, typeof note.id, id, typeof id, note.id === id)
+    return note.id === id
+  })
   console.log(note)
   response.json(note)
 })
