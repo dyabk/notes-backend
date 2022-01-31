8d72b32a853710a8f91d88b8be76f21dbac6f6ed
dyabk <dzmitry.yabkevich@outlook.com>
2022-01-31 06:55:33 -0500

    Switched the order of functions for testing purposes

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -36,15 +36,30 @@ let notes = [
     }
 ]
 
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if(!body.content) {
+  if (!body.content) {
     return response.status(400).json({
       error: 'content missing'
     })
   }
-  
+
   const note = {
     content: body.content,
     important: body.important || false,
@@ -57,18 +72,10 @@ app.post('/api/notes', (request, response) => {
   response.json(note)
 })
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/notes', (request, response) => {
-    response.json(notes)
-})
-
 app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   const note = notes.find(note => note.id === id)
-  
+
   if (note) {
     response.json(note)
   } else {
@@ -83,13 +90,6 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n >= n.id))
-    : 0
-  return maxId + 1
-}
-
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
@@ -98,5 +98,5 @@ app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
