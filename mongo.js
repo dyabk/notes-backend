a1e77099d2814f6596be52b0b8245d574c00eb9d
dyabk <dzmitry.yabkevich@outlook.com>
2022-02-02 11:19:34 -0500

    Wrote the code to establish the connection to the MongoDB database, with the user password being accepted as a command-line input argument

--- mongo.js
+++ mongo.js
@@ -1,1 +1,13 @@
 const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+    `mongodb+srv://majabaza:${password}@cluster0.k9dqp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+mongoose.connect(url)
