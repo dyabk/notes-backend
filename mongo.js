5bbbe620a2712d57fa144b4eb4140de6ec071989
dyabk <dzmitry.yabkevich@outlook.com>
2022-02-02 11:20:50 -0500

    Added the rest of the code for the test database

--- mongo.js
+++ mongo.js
@@ -11,3 +11,22 @@ const url =
     `mongodb+srv://majabaza:${password}@cluster0.k9dqp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
 mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    date: Date,
+    important: Boolean,
+})
+  
+const Note = mongoose.model('Note', noteSchema)
+  
+const note = new Note({
+    content: 'HTML is Easy',
+    date: new Date(),
+    important: true,
+})
+  
+note.save().then(result => {
+    console.log('note saved!')
+    mongoose.connection.close()
+})
