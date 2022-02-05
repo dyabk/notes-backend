7887cf4f47a5ce50888c07b132558196def48a25
dyabk <dzmitry.yabkevich@outlook.com>
2022-02-04 21:19:08 -0500

    Fixed the order of middleware execution, with the json-parser loading among the virst ones and the error handler being among the last ones to load

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const cors = require('cors')
-
-app.use(cors())
-app.use(express.json())
-app.use(express.static('build'))
-
 const Note = require('./models/note')
 
 const requestLogger = (request, response, next) => {
@@ -17,7 +11,35 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+app.use(express.static('build'))
+app.use(express.json())
 app.use(requestLogger)
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (body.content === undefined) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  }
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
@@ -62,26 +84,6 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-
-  if (body.content === undefined) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
-
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-  }
-
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
-})
-
 app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
     .then(note => {
@@ -100,12 +102,6 @@ app.delete('/api/notes/:id', (request, response) => {
 
   response.status(204).end()
 })
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(unknownEndpoint)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
