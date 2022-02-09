f02db621ef7b8f9341e5665f69b62460c18544a8
dyabk <dzmitry.yabkevich@outlook.com>
2022-02-08 21:30:48 -0500

    Defined the validation rules for the content and date fields in the mongoose schema to check the format before an object is stored in the database

--- mongo.js
+++ mongo.js
@@ -13,9 +13,16 @@ const url =
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean,
+    content: {
+        type: String,
+        minLength: 5,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    important: Boolean
 })
   
 const Note = mongoose.model('Note', noteSchema)
