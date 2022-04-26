d935e7995e9a80ecba914e83538298b5779a5250
dyabk <dzmitry.yabkevich@outlook.com>
2022-04-25 20:09:32 -0400

    Simplified index.js and moved the majority of its contents into app.js, the actual Express application

--- mongo.js
+++ mongo.js
@@ -1,33 +1,32 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 if (process.argv.length < 3) {
-    console.log('Please provide the password as an argument: node mongo.js <password>')
-    process.exit(1)
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
-const url =
-    `mongodb+srv://majabaza:${password}@cluster0.k9dqp.mongodb.net/note-app?retryWrites=true&w=majority`
+const url = `mongodb+srv://majabaza:${password}@cluster0.k9dqp.mongodb.net/note-app?retryWrites=true&w=majority`;
 
-mongoose.connect(url)
+const Note = mongoose.model("Note", noteSchema);
 
-const Note = mongoose.model('Note', noteSchema)
-  
 const note = new Note({
-    content: 'The last note',
-    date: new Date(),
-    important: true,
-})
-  
+  content: "The last note",
+  date: new Date(),
+  important: true,
+});
+
 //note.save().then(result => {
 //    console.log('note saved!')
 //    mongoose.connection.close()
 //})
 
-Note.find({}).then(result => {
-    result.forEach(note => {
-        console.log(note)
-    })
-    mongoose.connection.close()
-})
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
